07bc9f9326927f7f4bcfe45e5c689b384eb8d43f
dan-011 <daniel.paliulis@uconn.edu>
2023-04-16 15:28:06 -0400

    v1

---
+++ app/src/main/java/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package api
+
+import retrofit2.http.GET
+
+private const val API_KEY = "11228a78659d68f17627ecdc6c80fe0b"
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=$API_KEY" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): FlickrResponse
+}
